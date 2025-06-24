e1ea2e8acd91a31fc417b69627bde91524855bb7
Nerdzter <nayersonoliveira.contato@gmail.com>
2025-06-24 16:39:49 -0300

    estrutura

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:wakelock/wakelock.dart';
+import 'pages/home_page.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final cameras = await availableCameras();
+
+  runApp(MyApp(cameras: cameras));
+}
+
+class MyApp extends StatelessWidget {
+  final List<CameraDescription> cameras;
+  const MyApp({Key? key, required this.cameras}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Heart Rate PPG',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: HomePage(cameras: cameras),
+    );
+  }
+}
